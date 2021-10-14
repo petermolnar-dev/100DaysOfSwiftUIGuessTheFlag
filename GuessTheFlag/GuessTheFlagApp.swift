bf6568ac72e958857867f68e5ea9e9e3dc57b9f0
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-14 11:15:50 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Peter Molnar on 14/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
