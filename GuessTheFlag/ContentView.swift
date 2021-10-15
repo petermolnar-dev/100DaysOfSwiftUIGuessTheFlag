ec60bb1f77f11bbbf2dedd9a75e571ecb9679c69
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-15 16:14:12 +0200

    Completed Day 22 challenges: Adding the score, showing on a label, and notifying the user on a wrong selection.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,6 +14,21 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var userScore = 0
+    @State private var userSelected = 0
+    
+    var alertMessage: String {
+        if isCorrectAnswer {
+            return "Your score is: \(userScore)"
+        } else {
+            return "That’s the flag of \(countries[userSelected])"
+        }
+    }
+    
+    var isCorrectAnswer: Bool {
+        userSelected == correctAnswer
+    }
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
@@ -30,7 +45,8 @@ struct ContentView: View {
                 
                 ForEach(0 ..< 3) { number in
                     Button (action: {
-                        self.flagTapped(number)
+                        userSelected = number
+                        self.flagTapped()
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -39,24 +55,29 @@ struct ContentView: View {
                             .shadow(color: .black, radius: 2)
                     }
                 }
+                Text("Your score: \(userScore)")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
                 Spacer()
             }
             
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ??"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text(alertMessage), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
     }
     
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
+    
+    func flagTapped() {
+        if isCorrectAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong!"
         }
-        
         showingScore = true
     }
     
